c137f705707d8ed9ac7cb7e9742a53813de1e32e
micheal-ndoh <michaelndoh9@gmail.com>
2025-01-31 15:50:25 +0100

    Added dockerfile

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io;
 use std::fs::OpenOptions;
-use std::io::prelude::*;
+use std::io::Write;
 
 fn main() {
     loop {
@@ -16,15 +16,15 @@ fn main() {
             Ok(n) => n,
             Err(_) => {
                 println!("Invalid input for the first number.");
-                continue;
+                return;
             }
         };
 
         let num2: f64 = match num2.trim().parse() {
             Ok(n) => n,
             Err(_) => {
-                println!("Invalid on the second number.");
-                continue;
+                println!("Invalid input for the second number.");
+                return;
             }
         };
 
@@ -37,7 +37,7 @@ fn main() {
         } else if operation.trim() == "/" {
             if num2 == 0.0 {
                 println!("number cannot be divided by zero");
-                continue;
+                return;
             } else {
                 num1 / num2
             }
@@ -49,10 +49,10 @@ fn main() {
             factorial(num1 as u64) as f64
         } else {
             println!("Invalid operation");
-            continue;
+            return;
         };
 
-        print!(" {} {} {} = {}", num1, operation, num2, answer);
+        println!("The answer to {} {} {} is {}", num1, operation, num2, answer);
         log(num1, &operation, num2, answer);
     }
 }
@@ -60,12 +60,12 @@ fn main() {
 fn input(prompt: &str) -> String {
     println!("{}", prompt);
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read");
+    io::stdin().read_line(&mut input).expect("Failed to read line");
     input
 }
 
 fn log(num1: f64, operation: &str, num2: f64, answer: f64) {
-    let log_entry = format!("{} {} {} = {}", num1, operation, num2, answer);
+    let log_entry = format!("{} {} {} = {}\n", num1, operation, num2, answer);
     let mut file = OpenOptions::new()
         .append(true)
         .create(true)
@@ -74,10 +74,10 @@ fn log(num1: f64, operation: &str, num2: f64, answer: f64) {
     file.write_all(log_entry.as_bytes()).expect("Cannot write to file");
 }
 
-fn factorial(num1: u64) -> u64 {
-    if num1 == 0 {
+fn factorial(n: u64) -> u64 {
+    if n == 0 {
         1
     } else {
-        num1 * factorial(num1 - 1)
+        n * factorial(n - 1)
     }
 }
