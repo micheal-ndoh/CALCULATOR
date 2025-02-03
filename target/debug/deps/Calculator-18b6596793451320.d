1bfef287b7db9b680c6b60374372b3da9719500d
micheal-ndoh <michaelndoh9@gmail.com>
2025-02-03 02:31:08 +0100

    feature: added a struct filee

--- target/debug/deps/Calculator-18b6596793451320.d
+++ target/debug/deps/Calculator-18b6596793451320.d
@@ -1,6 +1,6 @@
-/home/micheal-ndoh/Projects/PROGRAMMING COURSES/RUST/project3/Calculator/target/debug/deps/Calculator-18b6596793451320: src/main.rs src/add.rs src/subtract.rs src/multiply.rs src/divide.rs src/power.rs src/factorial.rs src/modulus.rs
+/home/micheal-ndoh/Projects/PROGRAMMING COURSES/RUST/project3/Calculator/target/debug/deps/Calculator-18b6596793451320: src/main.rs src/add.rs src/subtract.rs src/multiply.rs src/divide.rs src/power.rs src/factorial.rs src/modulus.rs src/calculator.rs
 
-/home/micheal-ndoh/Projects/PROGRAMMING COURSES/RUST/project3/Calculator/target/debug/deps/Calculator-18b6596793451320.d: src/main.rs src/add.rs src/subtract.rs src/multiply.rs src/divide.rs src/power.rs src/factorial.rs src/modulus.rs
+/home/micheal-ndoh/Projects/PROGRAMMING COURSES/RUST/project3/Calculator/target/debug/deps/Calculator-18b6596793451320.d: src/main.rs src/add.rs src/subtract.rs src/multiply.rs src/divide.rs src/power.rs src/factorial.rs src/modulus.rs src/calculator.rs
 
 src/main.rs:
 src/add.rs:
@@ -10,3 +10,4 @@ src/divide.rs:
 src/power.rs:
 src/factorial.rs:
 src/modulus.rs:
+src/calculator.rs:
--- target/debug/deps/Calculator-297f66ec027453fb.d
+++ target/debug/deps/Calculator-297f66ec027453fb.d
@@ -1,6 +1,6 @@
-/home/micheal-ndoh/Projects/PROGRAMMING COURSES/RUST/project3/Calculator/target/debug/deps/libCalculator-297f66ec027453fb.rmeta: src/main.rs src/add.rs src/subtract.rs src/multiply.rs src/divide.rs src/power.rs src/factorial.rs src/modulus.rs
+/home/micheal-ndoh/Projects/PROGRAMMING COURSES/RUST/project3/Calculator/target/debug/deps/libCalculator-297f66ec027453fb.rmeta: src/main.rs src/add.rs src/subtract.rs src/multiply.rs src/divide.rs src/power.rs src/factorial.rs src/modulus.rs src/calculator.rs
 
-/home/micheal-ndoh/Projects/PROGRAMMING COURSES/RUST/project3/Calculator/target/debug/deps/Calculator-297f66ec027453fb.d: src/main.rs src/add.rs src/subtract.rs src/multiply.rs src/divide.rs src/power.rs src/factorial.rs src/modulus.rs
+/home/micheal-ndoh/Projects/PROGRAMMING COURSES/RUST/project3/Calculator/target/debug/deps/Calculator-297f66ec027453fb.d: src/main.rs src/add.rs src/subtract.rs src/multiply.rs src/divide.rs src/power.rs src/factorial.rs src/modulus.rs src/calculator.rs
 
 src/main.rs:
 src/add.rs:
@@ -10,3 +10,4 @@ src/divide.rs:
 src/power.rs:
 src/factorial.rs:
 src/modulus.rs:
+src/calculator.rs:
